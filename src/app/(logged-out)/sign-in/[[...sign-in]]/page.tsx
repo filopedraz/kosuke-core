c83f809872ed4ef4487b1d9ac77b7b2e350a97c3
Sergi <sergi.tantinya@gmail.com>
2025-10-27 12:38:18 +0100

    Fix/middleware (#219)

--- src/app/(logged-out)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(logged-out)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 export default function SignInPage() {
   return (
     <div className="absolute inset-0 top-14 flex items-center justify-center px-4">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" redirectUrl="/projects" />
+      <SignIn />
     </div>
   );
 }
