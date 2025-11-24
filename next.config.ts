d1711d41a03814b33ece5d81ef74c77994cf5280
Sergi <sergi.tantinya@gmail.com>
2025-11-24 14:16:35 +0100

    Fix headers (#268)
    * Fix headers
    
    * Fix headers

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@docker/node-sdk'],
   output: 'standalone',
+  poweredByHeader: false,
   experimental: {
     externalDir: true,
     webpackMemoryOptimizations: true,
